6763d627d461bfcdddf35ff0ad77d315a693117b
Paul Zabin <pablo.casabas@gmail.com>
2020-08-03 17:27:41 -0700

    updating fav icon

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p data-metaphor="link">
+          RocketBooster
         </p>
         <a
           className="App-link"
