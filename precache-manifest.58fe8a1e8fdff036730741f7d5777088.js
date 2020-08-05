62fa2efc05c9b19d15964df94cd43e46b2e60f40
Paul Zabin <pablo.casabas@gmail.com>
2020-08-04 20:52:02 -0700

    Updates

---
+++ precache-manifest.58fe8a1e8fdff036730741f7d5777088.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ead18aea96998757262875d34a47fe",
+    "url": "/rocket-booster/index.html"
+  },
+  {
+    "revision": "812eab69de8a85da8a4c",
+    "url": "/rocket-booster/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "910353f6b509cd712db5",
+    "url": "/rocket-booster/static/css/main.b5ce677f.chunk.css"
+  },
+  {
+    "revision": "812eab69de8a85da8a4c",
+    "url": "/rocket-booster/static/js/2.038faf68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rocket-booster/static/js/2.038faf68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910353f6b509cd712db5",
+    "url": "/rocket-booster/static/js/main.c1ff99c0.chunk.js"
+  },
+  {
+    "revision": "a784c58dfca6730e5bff",
+    "url": "/rocket-booster/static/js/runtime-main.05cf8cfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rocket-booster/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rocket-sled/precache-manifest.823aced8a3f0ecb2658f152738fd5ccf.js"
+  "/rocket-booster/precache-manifest.58fe8a1e8fdff036730741f7d5777088.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rocket-sled/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rocket-booster/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
