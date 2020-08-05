5c7ee4fc8f77e970047b4c47dd2acd6bc87c3bee
Paul Zabin <pablo.casabas@gmail.com>
2020-08-04 17:39:14 -0700

    adding initial demo build

---
+++ build/precache-manifest.1d48b1739e8b62e61f7226910e19f098.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7401ab02776570a8f8bebfd308cd948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "812eab69de8a85da8a4c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bf171d4f304b67543191",
+    "url": "/static/css/main.5f433a49.chunk.css"
+  },
+  {
+    "revision": "812eab69de8a85da8a4c",
+    "url": "/static/js/2.038faf68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.038faf68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf171d4f304b67543191",
+    "url": "/static/js/main.40168245.chunk.js"
+  },
+  {
+    "revision": "b78c86a26a1384ec70ba",
+    "url": "/static/js/runtime-main.285e1294.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
