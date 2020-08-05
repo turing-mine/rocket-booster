7013e0b4f475659627a8746af5b2efcc3fbd5575
Paul Zabin <pablo.casabas@gmail.com>
2020-08-04 20:17:50 -0700

    Updates

---
+++ precache-manifest.c9527f83c2c521769014c85b346a6e4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bae3c512c543a2439277fb19c5bbd83",
+    "url": "/rocket-sled/index.html"
+  },
+  {
+    "revision": "812eab69de8a85da8a4c",
+    "url": "/rocket-sled/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "13304b048afe20dc60c2",
+    "url": "/rocket-sled/static/css/main.b5ce677f.chunk.css"
+  },
+  {
+    "revision": "812eab69de8a85da8a4c",
+    "url": "/rocket-sled/static/js/2.038faf68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rocket-sled/static/js/2.038faf68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13304b048afe20dc60c2",
+    "url": "/rocket-sled/static/js/main.c969c38b.chunk.js"
+  },
+  {
+    "revision": "67701621b52a0ad72f61",
+    "url": "/rocket-sled/static/js/runtime-main.509ba148.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rocket-sled/static/media/logo.5d5d9eef.svg"
+  }
+]);
