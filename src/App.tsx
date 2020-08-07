8f75433092158386ab72efc1b195fc67722bc68f
Paul Zabin <pablo.casabas@gmail.com>
2020-08-06 22:19:55 -0700

    GHP deployment test

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          ¡Learn React!
         </a>
       </header>
     </div>
