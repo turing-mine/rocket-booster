62b85a02b38c297d3b8fcb6f5a6e2e4a2bfa0e43
Paul Zabin <pablo.casabas@gmail.com>
2020-08-10 19:20:23 -0700

    adding tabular component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import './App.scss'
 import Header from './components/header/Header'
+import Tabular from './components/tabular/Tabular'
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Tabular/>
     </div>
   )
 }
---
+++ src/components/tabular/Tabular.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './Tabular.scss'
+
+function Tabular() {
+    return (
+        <div className="Tabular">
+            Tabular/Flex
+        </div>
+    )
+}
+
+export default Tabular
+
+ 
