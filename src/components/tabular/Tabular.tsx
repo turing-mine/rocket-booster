8ec02144d6d9d7e61ffd0a37a5425ae577cee301
Paul Zabin <pablo.casabas@gmail.com>
2020-08-10 22:51:01 -0700

    adding responsive table

--- src/components/tabular/Tabular.tsx
+++ src/components/tabular/Tabular.tsx
@@ -5,7 +5,27 @@ import './Tabular.scss'
 function Tabular() {
     return (
         <div className="Tabular">
-            Tabular/Flex
+            <table>
+                <thead>
+                    <tr>
+                        <th>Ticker (Name)</th>
+                        <th>Close</th>
+                        <th>Dividend</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>KO (Coca Cola)</td>
+                        <td>$47.72</td>
+                        <td>$1.618</td>
+                    </tr>
+                    <tr>
+                        <td>AMT (American Tower)</td>
+                        <td>Tick</td>
+                        <td>$3.825</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
