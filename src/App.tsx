57d946bd5cf1fee20fc4114fd48eb279d4f9e796
Paul Zabin <pablo.casabas@gmail.com>
2020-08-11 19:31:17 -0700

    adding MobX observables to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 
 import React from 'react'
+import { observable } from "mobx"
+
 import './App.scss'
+
 import Header from './components/header/Header'
 import Tabular from './components/tabular/Tabular'
 import Dashboard from './components/dashboard/Dashboard'
 
 interface AppState {
     themeMode: string
+}
+
+class uiSetting {
+    @observable themeMode = "default"
 }
 
 class App extends React.Component<{}, AppState> {
